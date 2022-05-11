6e4c5ef4d0c0a434833f761bce7e69fbb3ad5435
Matt Glaman <nmd.matt@gmail.com>
2022-05-11 15:33:55 -0500

    Update Ch09 recipe 1 code example

--- 09/recipe1/mymodule/src/Entity/Recipe.php
+++ 09/recipe1/mymodule/src/Entity/Recipe.php
@@ -4,12 +4,20 @@ namespace Drupal\mymodule\Entity;
 
 use Drupal\node\Entity\Node;
 
-class Article extends Node {
+class Recipe extends Node {
 
   public function getTags(): array {
     /** @var \Drupal\Core\Field\EntityReferenceFieldItemListInterface $field_tags */
     $field_tags = $this->get('field_tags');
     return $field_tags->referencedEntities();
+  }
+
+  public function getPrepTime(): int {
+    return (int) $this->get('field_preparation_time')->value;
+  }
+
+  public function getCookTime(): int {
+    return (int) $this->get('field_cooking_time')->value;
   }
 
 }
